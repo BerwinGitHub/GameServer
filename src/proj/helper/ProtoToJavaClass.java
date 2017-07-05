7d914d37d6d0df1b559db22832aed2a81989f39e
Berwin <berwin.net@gmail.com>
2017-07-06 00:16:02 +0800

    添加到Eclipse

--- src/proj/helper/ProtoToJavaClass.java
+++ src/proj/helper/ProtoToJavaClass.java
@@ -3,19 +3,18 @@ package proj.helper;
 import java.io.File;
 import java.io.IOException;
 
-import com.berwin.proto.PersonEntity;
-import com.berwin.proto.PersonEntity.Person;
 import com.google.protobuf.InvalidProtocolBufferException;
 
 public class ProtoToJavaClass {
 
-	public static final String PROTOC = "./res/proto/protobuf/protoc.exe";
+	public static final String PROTOC_WIN = "./res/proto/protobuf/protoc.exe";
+	public static final String PROTOC_MAC = "protoc";
 	public static final String PACKAGE = "com/berwin/proto";
 
 	public static void main(String[] args) {
-		// listFile(new File("./res/proto"));
-		testProto();
-		System.out.println("执行完毕");
+		 listFile(new File("./res/proto"));
+//		testProto();
+		System.out.println("Finish");
 	}
 
 	public static void listFile(File f) {
@@ -23,41 +22,53 @@ public class ProtoToJavaClass {
 		for (int i = 0; i < fs.length; i++) {
 			if (fs[i].isDirectory()) {
 				listFile(fs[i]);
-			} else { // 如果是文件就处理文件
+			} else {
 				String path = fs[i].getPath();
 				if (path.endsWith(".proto")) {
-					String cmd = PROTOC + " -I=./res/proto --java_out=./src " + path;
+					// protoc -I=src/main/resource/proto
+					// --java_out=src/main/java
+					// src/main/resource/proto/protobuf.proto
+					String cmd = "";
+					String os = System.getProperty("os.name");
+					if (os.equals("Mac OS") || os.equals("Mac OS X")) {
+						cmd = PROTOC_MAC + " -I=./res/proto --java_out=./src"
+								+ path;
+					} else if (os.equals("Windows")) {
+						cmd = PROTOC_WIN + " -I=./res/proto --java_out=./src "
+								+ path;
+					}
 					System.out.println(cmd);
 					try {
 						Runtime.getRuntime().exec(cmd);
 					} catch (IOException e) {
 						e.printStackTrace();
-					} // 通过执行cmd命令调用protoc.exe程序
+					} //
 				}
 			}
 		}
 	}
 
 	public static void testProto() {
-		PersonEntity.Person.Builder builder = PersonEntity.Person.newBuilder();
-		builder.setId(0001);
-		builder.setName("Berwin");
-		builder.setEmail("berwin.net@gmail.com");
-		PersonEntity.Person person = builder.build();
-		byte[] byteArray = person.toByteArray();
-		System.out.println("[proto -> bytes]:");
-		for (int i = 0; i < byteArray.length; i++) {
-			System.out.print(byteArray[i]);
-		}
-		System.out.println("\r\n");
-		// byte => proto
-
-		try {
-			Person person2 = Person.parseFrom(byteArray);
-			System.out.println("[bytes -> proto]:\r\n" + person2.toString());
-		} catch (InvalidProtocolBufferException e) {
-			e.printStackTrace();
-		}
+		// PersonEntity.Person.Builder builder =
+		// PersonEntity.Person.newBuilder();
+		// builder.setId(0001);
+		// builder.setName("Berwin");
+		// builder.setEmail("berwin.net@gmail.com");
+		// PersonEntity.Person person = builder.build();
+		// byte[] byteArray = person.toByteArray();
+		// System.out.println("[proto -> bytes]:");
+		// for (int i = 0; i < byteArray.length; i++) {
+		// System.out.print(byteArray[i]);
+		// }
+		// System.out.println("\r\n");
+		// // byte => proto
+		//
+		// try {
+		// Person person2 = Person.parseFrom(byteArray);
+		// System.out.println("[bytes -> proto]:\r\n" + person2.toString());
+		// } catch (InvalidProtocolBufferException e) {
+		// e.printStackTrace();
+		// }
 
 	}
 
